5dc5a34f500da0e2203772a52c9b3573279c9c78
tombuildsstuff <git@tom.ibuildstuff.eu>
2022-04-19 14:17:14 +0200

    r/service_fabric_managed_cluster: removing overridden code

--- internal/services/servicefabricmanaged/service_fabric_managed_cluster_resource.go
+++ internal/services/servicefabricmanaged/service_fabric_managed_cluster_resource.go
@@ -18,7 +18,6 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tags"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/timeouts"
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
